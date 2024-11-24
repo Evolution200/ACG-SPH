6aeb67c760ec2c7efb894c9f4e1d9f405b70462d
Prince-Polo <1134012044@qq.com>
2024-11-24 15:53:34 +0800

    rigid

--- Simulation/Solver/base_solver.py
+++ Simulation/Solver/base_solver.py
@@ -1,10 +1,7 @@
 import taichi as ti
 import numpy as np
-import math
-import time
-import os
-import sys
 from ..Base import BaseContainer
+from .utils import *
 
 
 @ti.data_oriented
@@ -18,7 +15,7 @@ class BaseSolver():
 
         # density
         self.density = 1000.0
-        self.density0 = self.container.cfg.get_cfg("density0")
+        self.density_0 = self.container.cfg.get_cfg("density0")
 
         # surface tension
         if self.container.cfg.get_cfg("surface_tension"):
@@ -38,4 +35,162 @@ class BaseSolver():
         self.dt[None] = 1e-4
         self.dt[None] = self.container.cfg.get_cfg("timeStepSize")
 
+        # kernel
+        self.kernel = CubicKernel()
+
+        # others
+        if self.container.cfg.get_cfg("gravitationUpper"):
+            self.g_upper = self.container.cfg.get_cfg("gravitationUpper")
+        else:
+            self.g_upper = 10000.0
+
+    @ti.kernel
+    def compute_rigid_particle_volume(self):
+        # implemented as paper "Versatile Rigid-Fluid Coupling for Incompressible SPH"
+        for p_i in range(self.container.particle_num[None]):
+            if self.container.particle_materials[p_i] == self.container.material_rigid:
+                if self.container.particle_positions[p_i][1] <= self.g_upper:
+                    ret = self.kernel.weight(0.0, self.container.dh)
+                    self.container.for_all_neighbors(p_i, self.compute_rigid_particle_volumn_task, ret)
+                    self.container.particle_rest_volumes[p_i] = 1.0 / ret 
+                    self.container.particle_masses[p_i] = self.density_0 * self.container.particle_rest_volumes[p_i]
+
+    @ti.func
+    def compute_rigid_particle_volumn_task(self, p_i, p_j, ret: ti.template()):
+        pos_i = self.container.particle_positions[p_i]
+        if self.container.particle_object_ids[p_j] == self.container.particle_object_ids[p_i]:
+            pos_j = self.container.particle_positions[p_j]
+            R = pos_i - pos_j
+            R_mod = R.norm()
+            ret += self.kernel.weight(R_mod, self.container.dh)
+
+    @ti.kernel
+    def init_acceleration(self):
+        self.container.particle_accelerations.fill(0.0)
+
+    @ti.kernel
+    def init_rigid_body_force_and_torque(self):
+        self.container.rigid_body_forces.fill(0.0)
+        self.container.rigid_body_torques.fill(0.0)
+
+    @ti.kernel
+    def compute_pressure_acceleration(self):
+        self.container.particle_accelerations.fill(0.0)
+        for p_i in range(self.container.particle_num[None]):
+            if self.container.particle_is_dynamic[p_i]:
+                self.container.particle_accelerations[p_i] = ti.Vector([0.0 for _ in range(self.container.dim)])
+                if self.container.particle_materials[p_i] == self.container.material_fluid:
+                    ret_i = ti.Vector([0.0 for _ in range(self.container.dim)])
+                    self.container.for_all_neighbors(p_i, self.compute_pressure_acceleration_task, ret_i)
+                    self.container.particle_accelerations[p_i] = ret_i
+
+    @ti.func
+    def compute_pressure_acceleration_task(self, p_i, p_j, ret: ti.template()):
+        # compute pressure acceleration from the gradient of pressure
+        pos_i = self.container.particle_positions[p_i]
+        pos_j = self.container.particle_positions[p_j]
+        den_i = self.container.particle_densities[p_i]
+        R = pos_i - pos_j
+        nabla_ij = self.kernel.gradient(R, self.container.dh)
+
+        if self.container.particle_materials[p_j] == self.container.material_fluid:
+            den_j = self.container.particle_densities[p_j]
+            pressure_term = (self.container.particle_pressures[p_i] / (den_i * den_i) + self.container.particle_pressures[p_j] / (den_j * den_j))
+            ret += -self.container.particle_masses[p_j] * pressure_term * nabla_ij
+
+        elif self.container.particle_materials[p_j] == self.container.material_rigid:
+            # use fluid particle pressure, density as rigid particle pressure, density
+            den_j = self.density_0
+            pressure_term = self.container.particle_pressures[p_i] / (den_i * den_i)
+            ret += -self.density_0 * self.container.particle_rest_volumes[p_j] * pressure_term * nabla_ij
+
+            if self.container.particle_is_dynamic[p_j]:
+                # add force and torque to rigid body
+                object_j = self.container.particle_object_ids[p_j]
+                center_of_mass_j = self.container.rigid_body_centers_of_mass[object_j]
+                force_j = self.density_0 * self.container.particle_rest_volumes[p_j] * pressure_term * nabla_ij * self.density_0 * self.container.particle_rest_volumes[p_i]
+                torque_j = ti.math.cross(pos_i - center_of_mass_j, force_j)
+                self.container.rigid_body_forces[object_j] += force_j
+                self.container.rigid_body_torques[object_j] += torque_j
+
+    def compute_non_pressure_acceleration(self):
+        # compute acceleration from gravity, surface tension and viscosity
+        self.compute_gravity_acceleration()
+        self.compute_surface_tension_acceleration()
+        self.compute_viscosity_acceleration()
         
+    @ti.kernel
+    def compute_gravity_acceleration(self):
+        # assign g to all fluid particles, not +=
+        for p_i in range(self.container.particle_num[None]):
+            if self.container.particle_materials[p_i] == self.container.material_fluid:
+                self.container.particle_accelerations[p_i] =  ti.Vector(self.g)
+
+    @ti.kernel
+    def compute_surface_tension_acceleration(self):
+        for p_i in range(self.container.particle_num[None]):
+            if self.container.particle_materials[p_i] == self.container.material_fluid:
+                a_i = ti.Vector([0.0 for _ in range(self.container.dim)])
+                self.container.for_all_neighbors(p_i, self.compute_surface_tension_acceleration_task, a_i)
+                self.container.particle_accelerations[p_i] += a_i
+
+    @ti.func
+    def compute_surface_tension_acceleration_task(self, p_i, p_j, ret: ti.template()):
+        pos_i = self.container.particle_positions[p_i]
+        if self.container.particle_materials[p_j] == self.container.material_fluid:
+            # Fluid neighbors
+            diameter2 = self.container.particle_diameter * self.container.particle_diameter
+            pos_j = self.container.particle_positions[p_j]
+            R = pos_i - pos_j
+            R2 = R.norm_sqr()
+            mass_ratio = self.container.particle_masses[p_j] / self.container.particle_masses[p_i]
+            if R2 > diameter2:
+                weight = self.kernel.weight(R.norm(), self.container.dh)
+            else:
+                weight = self.kernel.weight(self.container.particle_diameter, self.container.dh)
+            ret -= self.surface_tension * mass_ratio * R * weight
+
+    @ti.kernel
+    def compute_viscosity_acceleration(self):
+        for p_i in range(self.container.particle_num[None]):
+            if self.container.particle_materials[p_i] == self.container.material_fluid:
+                a_i = ti.Vector([0.0 for _ in range(self.container.dim)])
+                self.container.for_all_neighbors(p_i, self.compute_viscosity_acceleration_task, a_i)
+                self.container.particle_accelerations[p_i] += (a_i / self.density_0)
+
+    @ti.func
+    def compute_viscosity_acceleration_task(self, p_i, p_j, ret: ti.template()):
+        pos_i = self.container.particle_positions[p_i]
+        pos_j = self.container.particle_positions[p_j]
+        R = pos_i - pos_j
+        nabla_ij = self.kernel.gradient(R, self.container.dh)
+        v_xy = ti.math.dot(self.container.particle_velocities[p_i] - self.container.particle_velocities[p_j], R)
+
+        if self.container.particle_materials[p_j] == self.container.material_fluid:
+            m_ij = (self.container.particle_masses[p_i] + self.container.particle_masses[p_j]) / 2
+            ret += (
+                2 * (self.container.dim + 2) * self.viscosity * m_ij 
+                / self.container.particle_densities[p_j]
+                / (R.norm()**2 + 0.01 * self.container.dh**2)  
+                * v_xy 
+                * nabla_ij
+            )
+
+        elif self.container.particle_materials[p_j] == self.container.material_rigid:
+            m_ij = (self.density_0 * self.container.particle_rest_volumes[p_j])
+            acc = (
+                2 * (self.container.dim + 2) * self.viscosity_b * m_ij
+                / self.container.particle_densities[p_i]
+                / (R.norm()**2 + 0.01 * self.container.dh**2)
+                * v_xy
+                * nabla_ij
+            )
+
+            ret += acc
+
+            if self.container.particle_is_dynamic[p_j]:
+                object_j = self.container.particle_object_ids[p_j]
+                center_of_mass_j = self.container.rigid_body_centers_of_mass[object_j]
+                force_j =  - acc * self.container.particle_masses[p_i] / self.density_0
+                self.container.rigid_body_forces[object_j] += force_j
+                self.container.rigid_body_torques[object_j] += ti.math.cross(pos_j - center_of_mass_j, force_j)
--- Simulation/Solver/rigid_solver.py
+++ Simulation/Solver/rigid_solver.py
@@ -1,9 +1,6 @@
 import taichi as ti
 import numpy as np
 import math
-import time
-import os
-import sys
 from typing import Tuple
 from ..Base import BaseContainer
 
@@ -14,27 +11,21 @@ class RigidSolver():
         self.cfg = container.cfg
         self.gravity = np.array(gravity)
         self.dt = dt
+        self.rigid_body_scales = ti.Vector.field(3, dtype=ti.f32, shape=10)
 
         self.current_time = 0.0
         self.current_step = 0
 
-        # rigid objects and blocks
+        # rigid objects
         self.current_objects = []
-        self.current_blocks = []
-        self.current_rigid = []
         self.rigid_objects = self.cfg.get_rigid_bodies()
-        self.rigid_blocks = self.cfg.get_rigid_blocks()
         num_objects = len(self.rigid_objects)
-        num_blocks = len(self.rigid_blocks)
-        num_rigid = num_objects + num_blocks
 
-        if num_rigid == 0:
-            print("No rigid object or block is defined.")
+        if num_objects == 0:
+            print("No rigid object is defined.")
         else:
             for rigid_objects in self.rigid_objects:
                 self.init_rigid_object(rigid_objects)
-            for rigid_blocks in self.rigid_blocks:
-                self.init_rigid_block(rigid_blocks)
 
         self.rigid_particle_temp_positions = ti.Vector.field(3, dtype=ti.f32, shape=10)
         self.rigid_body_temp_centers_of_mass = ti.Vector.field(3, dtype=ti.f32, shape=10)
@@ -43,16 +34,15 @@ class RigidSolver():
     def init_rigid_object(self, rigid_object):
         index = rigid_object["objectID"]
 
-        if index in self.current_rigid:
+        if index in self.current_objects:
             return
         if rigid_object["entryTime"] > self.total_time:
             return
         
         self.current_objects.append(index)
-        self.current_rigid.append(index)
         
-        is_static = not rigid_object["isDynamic"]
-        if is_static:
+        is_dynamic = not rigid_object["isDynamic"]
+        if not is_dynamic:
             return
         else:
             angle = rigid_object["rotationAngle"] / 360 * (2 * math.pi)
@@ -61,102 +51,42 @@ class RigidSolver():
             cx, cy, cz = np.cos(euler)
             sx, sy, sz = np.sin(euler)
 
+            self.rigid_body_scales[index] = np.array(rigid_object["scale"], dtype=np.float32)
             self.container.rigid_body_velocities[index] = np.array(rigid_object["velocity"], dtype=np.float32)
             self.container.rigid_body_angular_velocities[index] = np.array([0.0, 0.0, 0.0], dtype=np.float32)
             self.container.rigid_body_original_centers_of_mass[index] = np.array([0.0, 0.0, 0.0], dtype=np.float32)
             self.container.rigid_body_centers_of_mass[index] = np.array(rigid_object["translation"])
             self.container.rigid_body_rotations[index] = np.array([[cy * cz, -cy * sz, sy], [sx * sy * cz + cx * sz, -sx * sy * sz + cx * cz, -sx * cy], [-cx * sy * cz + sx * sz, cx * sy * sz + sx * cz, cx * cy]])
 
+    def create_boundary(self, thickness: float = 0.01):
+        # we do not want the rigid to hit the boundary of fluid so we move each wall inside a little bit.
+        eps = self.container.particle_diameter + self.container.domain_box_thickness 
+        domain_start = self.container.domain_start
+        domain_end = self.container.domain_end
 
-    def init_rigid_block(self, rigid_block):
-        raise NotImplementedError
-    
-    def apply_force(self, index, force: Tuple[float, float, float]):
-        self.container.rigid_body_forces[index] += np.array(force)
+        self.start = np.array(domain_start) + eps
+        self.end = np.array(domain_end) - eps
 
-    def apply_torque(self, index, torque: Tuple[float, float, float]):
-        self.container.rigid_body_torques[index] += np.array(torque)
+    def update_velocity(self, index):
+        self.container.rigid_body_velocities[index] += self.gravity * self.dt + self.container.rigid_body_forces[index] / self.container.rigid_body_masses[index] * self.dt
+
+    def update_angular_velocity(self, index):
+        self.container.rigid_body_angular_velocities[index] += self.container.rigid_body_torques[index] / (0.4 * self.container.rigid_body_masses[index] * self.rigid_body_scales[index].norm() ** 2) * self.dt
+
+    def update_position(self, index):
+        self.container.rigid_body_centers_of_mass[index] += self.container.rigid_body_velocities[index] * self.dt
+        rotation_vector = self.container.rigid_body_angular_velocities[index] * self.dt
+        theta = rotation_vector.norm()
+        omega = rotation_vector.normalized()
+        omega_cross = np.array([[0.0, -omega[2], omega[1]], [omega[2], 0.0, -omega[0]], [-omega[1], omega[0], 0.0]])
+        rotation_matrix = np.eye(3) + np.sin(theta) * omega_cross + (1 - np.cos(theta)) * omega_cross @ omega_cross
+        self.container.rigid_body_rotations[index] = rotation_matrix @ self.container.rigid_body_rotations[index]
 
     def step(self):
-        for container_id in range(self.container.object_num[None]):
-            if self.container.rigid_body_is_dynamic[container_id] and self.container.object_materials[container_id] == self.container.material_rigid:
-                force_i = self.container.rigid_body_forces[container_id]
-                torque_i = self.container.rigid_body_torques[container_id]
-                self.apply_force(container_id, force_i)
-                self.apply_torque(container_id, torque_i)
-                self.container.rigid_body_forces[container_id] = np.array([0.0, 0.0, 0.0])
-                self.container.rigid_body_torques[container_id] = np.array([0.0, 0.0, 0.0])
-                
-        self.update_rigid_body_states()
 
-    def update_rigid_body_states(self):
-        for container_id in range(self.container.object_num[None]):
-            if self.container.rigid_body_is_dynamic[container_id] and self.container.object_materials[container_id] == self.container.material_rigid:
-                state_i = self.get_rigid_body_states(container_id)
-                self.container.rigid_body_centers_of_mass[container_id] = state_i["position"]
-                self.container.rigid_body_rotations[container_id] = state_i["rotation_matrix"]
-                self.container.rigid_body_velocities[container_id] = state_i["linear_velocity"]
-                self.container.rigid_body_angular_velocities[container_id] = state_i["angular_velocity"]
-
-    def get_rigid_body_states(self, container_idx):
-        linear_velocity = self.container.rigid_body_velocities[container_idx]
-        angular_velocity = self.container.rigid_body_angular_velocities[container_idx]
-        position = self.container.rigid_body_centers_of_mass[container_idx]
-        rotation_matrix = self.container.rigid_body_rotations[container_idx]
-
-        return {
-            "linear_velocity": linear_velocity,
-            "angular_velocity": angular_velocity,
-            "position": position,
-            "rotation_matrix": rotation_matrix
-        }
-
-    @ti.kernel
-    def update_rigid_velocities(self):
-        for p_i in range(self.container.particle_num[None]):
-            if self.container.particle_materials[p_i] == self.container.material_rigid and self.container.particle_is_dynamic[p_i]:
-                self.container.particle_velocities[p_i] += self.dt[None] * self.container.particle_accelerations[p_i]
-
-    @ti.kernel
-    def update_rigid_positions(self):
-        for p_i in range(self.container.particle_num[None]):
-            if self.container.particle_materials[p_i] == self.container.material_rigid and self.container.particle_is_dynamic[p_i]:
-                self.rigid_particle_temp_positions[p_i] = self.container.particle_positions[p_i] + self.dt[None] * self.container.particle_velocities[p_i]
-
-    @ti.kernel
-    def compute_temp_center_of_mass(self):
-        self.rigid_body_temp_centers_of_mass.fill(0.0)
-
-        for p_i in range(self.container.particle_num[None]):
-            if self.container.particle_materials[p_i] == self.container.material_rigid and self.container.particle_is_dynamic[p_i]:
-                object_id = self.container.particle_object_ids[p_i]
-                self.rigid_body_temp_centers_of_mass[object_id] += self.container.V0 * self.container.particle_densities[p_i] * self.rigid_particle_temp_positions[p_i]
-
-        for obj_i in range(self.container.object_num[None]):
-            if self.container.rigid_body_is_dynamic[obj_i] and self.container.object_materials[obj_i] == self.container.material_rigid:
-                self.rigid_body_temp_centers_of_mass[obj_i] /= self.container.rigid_body_masses[obj_i]
-
-    @ti.kernel
-    def solve_constraints(self):
-        self.rigid_body_temp_rotation_matrices.fill(0.0)
-
-        for p_i in range(self.container.particle_num[None]):
-            if self.container.particle_materials[p_i] == self.container.material_rigid and self.container.particle_is_dynamic[p_i]:
-                object_id = self.container.particle_object_ids[p_i]
-                q = self.container.rigid_particle_original_positions[p_i] - self.container.rigid_body_original_centers_of_mass[object_id]
-                p = self.rigid_particle_temp_positions[p_i] - self.rigid_body_temp_centers_of_mass[object_id]
-                self.rigid_body_temp_rotation_matrices[object_id] += self.container.V0 * self.container.particle_densities[p_i] * p.outer_product(q)
-
-        for obj_i in range(self.container.object_num[None]):
-            if self.container.rigid_body_is_dynamic[obj_i] and self.container.object_materials[obj_i] == self.container.material_rigid:
-                A_pq = self.rigid_body_temp_rotation_matrices[obj_i]
-                R, S = ti.polar_decompose(A_pq)
-                if all(abs(R) < 1e-6):
-                    R = ti.Matrix.identity(ti.f32, 3)
-                self.rigid_body_temp_rotation_matrices[obj_i] = R
-
-        for p_i in range(self.container.particle_num[None]):
-            if self.container.particle_materials[p_i] == self.container.material_rigid and self.container.particle_is_dynamic[p_i]:
-                object_id = self.container.particle_object_ids[p_i]
-                goal = self.rigid_body_temp_centers_of_mass[object_id] + self.rigid_body_temp_rotation_matrices[object_id] @ (self.container.rigid_particle_original_positions[p_i] - self.container.rigid_body_original_centers_of_mass[object_id])
-                self.container.particle_positions[p_i] = goal
+        for index in range(self.container.object_num[None]):
+            self.update_velocity(index)
+            self.update_angular_velocity(index)
+            self.update_position(index)
+            self.container.rigid_body_forces[index] = np.array([0.0, 0.0, 0.0])
+            self.container.rigid_body_torques[index] = np.array([0.0, 0.0, 0.0])
